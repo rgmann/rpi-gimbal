058d080d66a9d03f8def28ef36ed72224b4a113c
Robert Glissmann <robert.glissmann@gmail.com>
2024-08-04 00:21:35 -0600

    Fixing copy paste error in control.

--- pan_tilt_controller.cpp
+++ pan_tilt_controller.cpp
@@ -179,10 +179,12 @@ bool PanTiltController::ease_position( float phi, float theta )
             }
 
             float nextTheta = current_theta_;
-            if ( !at_target_phi )
+            if ( !at_target_theta )
             {
-               nextPhi += kMaxRadPerSec * kControlIntervalMs * kMilliSecondsPerSecond * dir_mult_theta;
+               nextTheta += kMaxRadPerSec * kControlIntervalMs * kMilliSecondsPerSecond * dir_mult_theta;
             }
+
+            at_target = at_target_phi && at_target_theta;
 
             this->set_position(nextPhi, nextTheta);
 
